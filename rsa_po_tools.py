114c515fe1fe683c4695af35f1b38ffce632ce36
Will Bowditch <bowditcw@bc.edu>
2017-04-25 18:11:21 -0400

    basic post request for oracle

---
+++ rsa_po_tools.py
@@ -0,0 +1,28 @@
+
+import urllib
+import urllib2
+#routines for posting a request
+#to the RSA padding oracle servers
+
+
+
+#This makes a post request to the server.  The guess is
+#the ciphertext in hex. The argument which is the string
+#'small' or 'big' depending on which of the two servers you want to query.
+
+def post_dictionary_guess(guess,which):
+    url_2='http://cscicrypto.bc.edu:8080/'+which+'rsa'
+
+    values=[('Entry',guess)]
+    data = urllib.urlencode(values)
+    req = urllib2.Request(url_2, data)
+    rsp=urllib2.urlopen(req)
+    #print rsp.code
+
+    return rsp.read()
+
+
+
+
+
+def correctPadding(guess,which):
