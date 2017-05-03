67f14f76c89d83467dc1cb0de284204550e42d24
jpscaduto <jps128@gmail.com>
2017-05-03 09:56:31 -0400

    Add files via upload

--- padAttack.py
+++ padAttack.py
@@ -1,6 +1,7 @@
 import rsa_po_tools
 import conversions
 
+
 smallCiphertext = '5011ee363939c57ef857f85330db2a796b620145cbdaf20abb10c941436c421d'
 
 smallModulus = '5b72b8210267044f2436817bc3b49a3229bb81276f87eabb09a6ddaf9ccfc67f'
@@ -21,7 +22,7 @@ def as_to_int(initNum):
 	intNum = int(hexNum,16)
 	#print intNum,len(str(intNum))
 	return intNum
-
+#print as_to_int(smallModulus)
 
 def rsaDecrypt(c,d,n):
 	return pow(c,d,n)
@@ -30,6 +31,7 @@ def int_to_as(initNum):
 	hexNum = hex(initNum)[2:-1]
 	final = conversions.hex_to_as(hexNum)
 	return final
+
 def rsaEncrypt(m,e,n):
 	m = conversions.as_to_hex(m)
 	m = int(m,16)
@@ -42,7 +44,6 @@ def padding_attack(encExp,mod,ciphertext,k,which): #which is small or big
 	B = 2**(8*(k-2))
 	#print B, len(str(B))
 	Mi = set([(2 * B, 3 * B - 1)])
-	#print Mi
 	c_0 = as_to_int(ciphertext)		#blinding 1
 	s_i = 1							#blinding 1
 	count = 1
@@ -51,71 +52,59 @@ def padding_attack(encExp,mod,ciphertext,k,which): #which is small or big
 	while working:
 		siPrev = s_i	 			#step 2
 		miPrev = Mi
-		print(Mi, count)
+		
 		if count == 1:  			#2a
+			print "2.a"
 			s_i = n/(3*B)	#set s_1 as the first number above n/3B that has correct padding with C*(si)**e mod n
 			while rsa_po_tools.correctPadding(int_to_as(c_0 * pow(s_i,e,n)),which) == False:
 				s_i+=1
-		elif len(miPrev)>1: 			#2b
+		
+		elif len(miPrev)>1: 		#2b
 			print "2.b"
 			s_i =  siPrev + 1		
 			while rsa_po_tools.correctPadding(int_to_as(c_0 * pow(s_i,e,n)),which) == False:
 				s_i+=1			#set s_1 as the first number above previous si that has correct padding with C*(si)**e mod n
+		
 		else: 						#2c 
 			print "2.c"
 			a = list(miPrev)[0][0]
 			b = list(miPrev)[0][1]
-			#print a
-			#print b 
+
 			ri = 2 * ((b * siPrev - 2 * B) / n)
 			
 			searching = True
-
-			print "a:",a, count
-			print "b:",b , b-a
-			print "a", len(str(a)), "b",len(str(b))
 
 			while searching:
 				
 				s_i = (2 * B + ri * n) / b
 				maxSi = (3 * B + ri * n) / a
-				if maxSi-s_i < 0: return
-				print ri
+
+				#if maxSi-s_i < 0: return
+				
 				while s_i <= maxSi:
 					if rsa_po_tools.correctPadding(int_to_as(c_0 * pow(s_i,e,n)),which) == True:
 						searching = False		#if fits padding stop looking
 						break				#leave the while loop for iterating si
-					#print maxSi - s_i	
+		
 					s_i += 1		#iterate through all si in the range
 				
 				ri+=1 #iterate through each ri
-		print("Step 2 complete")
-		#print(s_i)
-		#print Mi
-		#print miPrev
+		print("Step 2 complete",count)
+		
 		Mi = set()					#begin step 3
 		for a,b in miPrev:
-			r= (a * s_i - 3 * B + 1)/ n 
+			r = (a * s_i - 3 * B + 1)/ n 
 			maxR = (b * s_i - 2 * B) / n 
-			#print (b * s_i - 2 * B)
-			#print n
-			
-			#print s_i
-			#print(len(str(a)),len(str(b)),len(str(B)),len(str(s_i)), len(str(n)))
-
-			print r,maxR
 			
 			while r <= maxR:
 				bottom = (((2*B)+(r*n))/s_i)
-				
 				bottom = max(a,bottom)
-				#upper = (((3*B)-1)+(r*n))/s_i
-				upper = min(b,((3 * B - 1 + (r * n))/s_i))
-				print Mi
+				upper = (((3*B)-1)+(r*n))/s_i
+				upper = min(b,upper)
 				Mi.add((bottom,upper))
 				r+=1
-				print Mi
-		print("step3 done")		
+				
+		print("step3 complete",count)		
 		
 		if len(Mi)==1: #begin step 4
 			a = list(Mi)[0][0]
@@ -128,9 +117,10 @@ def padding_attack(encExp,mod,ciphertext,k,which): #which is small or big
 				print m
 				return m
 		count += 1
+
 padding_attack(encExp,smallModulus,smallCiphertext,32,'small')
 
-
+#HAVE TO ADDRESS PROBLEM OF LARGE E
 
 '''You want to try this out on your own first by making a little 
 'oracle' that runs on your own machine, without worrying about 
