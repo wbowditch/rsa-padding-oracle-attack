db85784da9878e38a88f19a73e356b355e41a183
jpscaduto <jps128@gmail.com>
2017-05-03 14:59:41 -0400

    Add files via upload

--- padAttack.py
+++ padAttack.py
@@ -1,28 +1,34 @@
 import rsa_po_tools
 import conversions
+import string
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_v1_5
+from Crypto.Hash import SHA
+from Crypto.Util.number import bytes_to_long, long_to_bytes
+
+def to_int(initNum):
+	hexNum = initNum
+	intNum = int(hexNum,16)
+	return intNum
+#print to_i
 
 
-smallCiphertext = '5011ee363939c57ef857f85330db2a796b620145cbdaf20abb10c941436c421d'
+smallCiphertext = to_int('5011ee363939c57ef857f85330db2a796b620145cbdaf20abb10c941436c421d')
 
-smallModulus = '5b72b8210267044f2436817bc3b49a3229bb81276f87eabb09a6ddaf9ccfc67f'
+smallModulus = to_int('5b72b8210267044f2436817bc3b49a3229bb81276f87eabb09a6ddaf9ccfc67f')
 
-bigCiphertext='01f5467c585896d061af690e1272bbe982d12ae8d9a8e985030d50f04d838106cb25f7a9cd64c268a490105c58e226860550e171faf83781adb82ebcf3125cf7'
+bigCiphertext = to_int('01f5467c585896d061af690e1272bbe982d12ae8d9a8e985030d50f04d838106cb25f7a9cd64c268a490105c58e226860550e171faf83781adb82ebcf3125cf7')
 
-bigModulus='98c7aa8856a0bf43c74541648148482fb0e554bab2caa87a52b40f1b10ed29741af87b8dcba40a7bb2c0755943b4cc99cf0a776ab4e95fd3c2f96cd85311f7df'
+bigModulus = to_int('98c7aa8856a0bf43c74541648148482fb0e554bab2caa87a52b40f1b10ed29741af87b8dcba40a7bb2c0755943b4cc99cf0a776ab4e95fd3c2f96cd85311f7df')
 
 encExp = 65537
 
 
-
-pubkey = (65537,41363160597721029885472283321473316054104792687939226295669553333550772635263)
-
-def as_to_int(initNum):
-	hexNum = initNum#conversions.as_to_hex(initNum)
-	#print hexNum,len(hexNum)
+def to_int(initNum):
+	hexNum = initNum
 	intNum = int(hexNum,16)
-	#print intNum,len(str(intNum))
 	return intNum
-#print as_to_int(smallModulus)
+#print to_int(smallModulus)
 
 def rsaDecrypt(c,d,n):
 	return pow(c,d,n)
@@ -38,13 +44,19 @@ def rsaEncrypt(m,e,n):
 	return pow(m,e,n)
 
 
-def padding_attack(encExp,mod,ciphertext,k,which): #which is small or big
+
+def padding_attack(encExp,mod,ciphertext,which): #which is small or big
+	if which == 'small':
+		k = 256/8
+	else:
+		k = 512/8
+	
 	e = encExp
-	n = as_to_int(mod)
+	n = mod#to_int(mod)
 	B = 2**(8*(k-2))
-	#print B, len(str(B))
+	print B, len(str(B))
 	Mi = set([(2 * B, 3 * B - 1)])
-	c_0 = as_to_int(ciphertext)		#blinding 1
+	c_0 = ciphertext#to_int(ciphertext)		#blinding 1
 	s_i = 1							#blinding 1
 	count = 1
 	working = True
@@ -52,18 +64,25 @@ def padding_attack(encExp,mod,ciphertext,k,which): #which is small or big
 	while working:
 		siPrev = s_i	 			#step 2
 		miPrev = Mi
-		
 		if count == 1:  			#2a
 			print "2.a"
-			s_i = n/(3*B)	#set s_1 as the first number above n/3B that has correct padding with C*(si)**e mod n
-			while rsa_po_tools.correctPadding(int_to_as(c_0 * pow(s_i,e,n)),which) == False:
+			s_i = n /(3*B)	#set s_1 as the first number above n/3B that has correct padding with C*(si)**e mod n
+			print s_i, n, B
+			#print format(((c_0 * pow(s_i,e,n))),'x')
+			while rsa_po_tools.correctPadding(format(((c_0 * pow(s_i,e,n))),'x'),which) == False:
+			#while not fcrypt(c_0 * pow(s_i, e, n)):
+				#print s_i
 				s_i+=1
 		
+
 		elif len(miPrev)>1: 		#2b
 			print "2.b"
-			s_i =  siPrev + 1		
-			while rsa_po_tools.correctPadding(int_to_as(c_0 * pow(s_i,e,n)),which) == False:
+			s_i =  siPrev + 1	
+			print (c_0 * pow(s_i,e,n))
+			while rsa_po_tools.correctPadding(format(((c_0 * pow(s_i,e,n))),'x'),which) == False:
+			#while not fcrypt(c_0 * pow(s_i, e, n)):
 				s_i+=1			#set s_1 as the first number above previous si that has correct padding with C*(si)**e mod n
+				print s_i
 		
 		else: 						#2c 
 			print "2.c"
@@ -79,10 +98,12 @@ def padding_attack(encExp,mod,ciphertext,k,which): #which is small or big
 				s_i = (2 * B + ri * n) / b
 				maxSi = (3 * B + ri * n) / a
 
-				#if maxSi-s_i < 0: return
-				
+				if maxSi-s_i < 0: return
+				#print(maxSi-s_i)
+
 				while s_i <= maxSi:
-					if rsa_po_tools.correctPadding(int_to_as(c_0 * pow(s_i,e,n)),which) == True:
+					if rsa_po_tools.correctPadding(format(((c_0 * pow(s_i,e,n))),'x'),which) == True:
+					#if fcrypt(c_0 * pow(s_i, e, n)):
 						searching = False		#if fits padding stop looking
 						break				#leave the while loop for iterating si
 		
@@ -93,34 +114,42 @@ def padding_attack(encExp,mod,ciphertext,k,which): #which is small or big
 		
 		Mi = set()					#begin step 3
 		for a,b in miPrev:
-			r = (a * s_i - 3 * B + 1)/ n 
+			#print a,b
+			r, mod1 = divmod(((a * s_i) - ((3 * B) + 1)), n)
+			if mod1 != 0:
+				r+=1
 			maxR = (b * s_i - 2 * B) / n 
+			print (a*s_i) < (3*B+1)
 			
+			print(r,maxR)
+
 			while r <= maxR:
-				bottom = (((2*B)+(r*n))/s_i)
+				bottom,mod1 = divmod(((2*B)+(r*n)),s_i)
+				if mod1 != 0:
+					bottom +=1
 				bottom = max(a,bottom)
-				upper = (((3*B)-1)+(r*n))/s_i
+				upper = divmod((((3*B)-1)+(r*n)),s_i)[0]
 				upper = min(b,upper)
 				Mi.add((bottom,upper))
 				r+=1
 				
 		print("step3 complete",count)		
-		
+		print('a:',a,'b:',b, 'a-b:',a-b)
 		if len(Mi)==1: #begin step 4
 			a = list(Mi)[0][0]
 			b = list(Mi)[0][1]
-
+			print('a:',a,'b:',b, 'a-b:',a-b)
 			if a == b:
-				message = int_to_as(a)
+				message = long_to_bytes(a)
 
 				m = '\x00'*(k-len(message)) + message
 				print m
 				return m
 		count += 1
 
-padding_attack(encExp,smallModulus,smallCiphertext,32,'small')
+#padding_attack(encExp,smallModulus,smallCiphertext,'small')
 
-#HAVE TO ADDRESS PROBLEM OF LARGE E
+
 
 '''You want to try this out on your own first by making a little 
 'oracle' that runs on your own machine, without worrying about 
@@ -131,3 +160,21 @@ Since you will need to query the server many times, you are not
 going to manually enter ciphertexts in the form.  
 Instead, I've provided you with a tool for posting a query 
 to the server, attached to this e-mail.'''
+
+
+def try1():
+	bits = 256
+	k = bits/8
+	pubkey = smallModulus
+	c = smallCiphertext
+	pm = padding_attack(65537,pubkey,c,'small')
+	'''
+	bits = 512
+	k = bits/8
+	pubkey = bigModulus
+	c = bigCiphertext
+	pm = padding_attack(65537,pubkey,c,'big')
+	'''
+	#print 'Recovered: ', repr(pm)
+	#print 'Match:', pm == pmsg
+try1()
